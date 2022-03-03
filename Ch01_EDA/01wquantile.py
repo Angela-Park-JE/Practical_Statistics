37574eb12a4fb8e10f999bf5bbe8a91721aaf9d5
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-03-03 20:40:09 +0900

    Create 01wquantile.py

---
+++ Ch01_EDA/01wquantile.py
@@ -0,0 +1,21 @@
+# 가중 중간값 구하기
+
+# wquantiles 라는 패키지가 !pip install로 안깔리길래 순망치한으로 직접 만들어본 함수인다.
+
+def wquantile(dataframe, column_name, weight):
+    # please write the column_name with ''
+    import numpy as pd
+    
+    dataframe[column_name].sort_values()
+    dataframe.reindex()
+    
+    cut = round((dataframe[column_name].count()) * weight)
+    #cut = round((dataframe[column_name].count()) / weight)
+    df = dataframe[cut:-cut]
+    
+    return np.median(df[column_name])   
+
+# 결과는 틀렸다. 웨이트값을 직접 주는 것으로 (trim=0.1)을 차용하려고 했으나 원래 실습에서의 정답과 다른 답이 나왔다. 
+# 틀린 이유는 sort하는 컬럼과 구하는 값이 같아서 문제에서 원하는 답이 안나왔던 것이다.
+# 다시 짜보기로 했다. 
+
